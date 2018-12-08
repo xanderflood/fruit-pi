a5d6b7bbc735166c45836554fce9c889439ce6f4
xanderflood <xander.t.flood@gmail.com>
2018-12-08 13:34:43 -0500

    update config code

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,29 +5,39 @@ import (
 	"encoding/json"
 	"io"
 
-	"github.com/xanderflood/fruit-pi/lib/config"
+	"github.com/xanderflood/fruit-pi/pkg/am2301"
+	"github.com/xanderflood/fruit-pi/pkg/chamber"
+	"github.com/xanderflood/fruit-pi/pkg/gpio"
+	"github.com/xanderflood/fruit-pi/pkg/relay"
 )
 
+//Config general configuration struct
 type Config struct {
-	FanPin    int             `json:"fan_pin"`
-	HumPin    int             `json:"hum_pin"`
-	SensorPin int             `json:"sensor_pin"`
-	Interval  config.Duration `json:"interval"`
-	Strategy  StrategyConfig  `json:"strategy"`
+	Pins     PinMapping     `json:"pins"`
+	Strategy StrategyConfig `json:"strategy"`
 }
 
-func Load(r io.Reader) (Config, error) {
+//PinMapping pins
+type PinMapping struct {
+	Fan    int `json:"fan"`
+	Hum    int `json:"hum"`
+	Sensor int `json:"sensor"`
+}
+
+//Load load configuration from a stream
+func Load(r io.Reader) (*Config, error) {
 	buf := bytes.NewBuffer([]byte{})
 	_, err := io.Copy(buf, r)
 	if err != nil {
-		return Config{}, err
+		return &Config{}, err
 	}
 
-	cfg := Config{}
-	err = json.Unmarshal(buf.Bytes(), &cfg)
+	cfg := &Config{}
+	err = json.Unmarshal(buf.Bytes(), cfg)
 	return cfg, err
 }
 
+//Apply load partial configuration from stream and merge
 func (cfg *Config) Apply(r io.Reader) error {
 	buf := bytes.NewBuffer([]byte{})
 	_, err := io.Copy(buf, r)
@@ -38,6 +48,7 @@ func (cfg *Config) Apply(r io.Reader) error {
 	return json.Unmarshal(buf.Bytes(), cfg)
 }
 
+//Save write configuration to stream
 func (cfg *Config) Save(w io.Writer) error {
 	data, err := json.Marshal(cfg)
 	if err != nil {
@@ -47,4 +58,13 @@ func (cfg *Config) Save(w io.Writer) error {
 	buf := bytes.NewBuffer(data)
 	_, err = io.Copy(w, buf)
 	return err
+}
+
+//Chamber build a chamber from this configuration
+func (cfg *Config) Chamber() chamber.Chamber {
+	return chamber.Chamber{
+		Sensor:     am2301.New(gpio.New(cfg.Pins.Sensor)),
+		Fan:        relay.New(gpio.New(cfg.Pins.Fan)),
+		Humidifier: relay.New(gpio.New(cfg.Pins.Hum)),
+	}
 }
--- pkg/config/strategy.go
+++ pkg/config/strategy.go
@@ -4,21 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 
+	perrors "github.com/pkg/errors"
+
 	"github.com/xanderflood/fruit-pi/pkg/chamber"
-	"github.com/xanderflood/fruit-pi/pkg/strategy"
 )
 
+//StrategyConfigContents StrategyConfigContents
 type StrategyConfigContents struct {
-	Name   string      `json:"name"`
-	Config interface{} `json:"config"`
+	Name   string            `json:"name"`
+	Config chamber.Validator `json:"config"`
 }
 
+//StrategyConfig StrategyConfig
 type StrategyConfig struct {
 	*StrategyConfigContents
 
 	Object chamber.Strategy
 }
 
+//UnmarshalJSON UnmarshalJSON
 func (s *StrategyConfig) UnmarshalJSON(b []byte) error {
 	if s.StrategyConfigContents == nil {
 		s.StrategyConfigContents = &StrategyConfigContents{}
@@ -26,35 +30,28 @@ func (s *StrategyConfig) UnmarshalJSON(b []byte) error {
 
 	contents := s.StrategyConfigContents
 
-	//unmarshals just the strategy name, since
-	//contents.Config is nil
+	//unmarshals just the strategy name, since contents.Config is nil
 	err := json.Unmarshal(b, contents)
 	if err != nil {
 		return err
 	}
 
 	//get a strategy object and its config object
-	s.Object = loadStrategy(contents.Name)
+	s.Object = chamber.Load(contents.Name)
 	if s.Object == nil {
 		return fmt.Errorf("invalid strategy name: %s", contents.Name)
 	}
 	contents.Config = s.Object.Configuration()
+	err = contents.Config.Validate()
+	if err != nil {
+		return perrors.Wrapf(err, "invalid config for strategy type `%s`", contents.Name)
+	}
 
-	//unmarshal again, this time with a non-nil
-	//contents.Config
+	//unmarshal again, this time with a non-nil contents.Config
 	err = json.Unmarshal(b, contents)
 	if err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func loadStrategy(name string) chamber.Strategy {
-	switch name {
-	case "standard":
-		return &strategy.Standard{}
-	default:
-		return nil
-	}
 }
