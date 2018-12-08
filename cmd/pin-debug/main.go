d6563879ce127ca2061fac814fbe100793182e14
xanderflood <xander.t.flood@gmail.com>
2018-12-08 13:36:54 -0500

    added pin-debug command

---
+++ cmd/pin-debug/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	flags "github.com/jessevdk/go-flags"
+	"github.com/xanderflood/fruit-pi/lib/tools"
+	"github.com/xanderflood/fruit-pi/pkg/gpio"
+)
+
+//Timeout defualt timeout for reading sequences
+const Timeout = time.Second
+
+var opts struct {
+	Pin int `long:"pin" env:"PIN" default:"22"`
+}
+
+//GPIOStates GPIO state names
+var GPIOStates = map[gpio.State]string{
+	gpio.Low:  "low",
+	gpio.High: "high",
+}
+
+func main() {
+	fmt.Println("-- Starting Fruit-Pi Client --")
+
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := gpio.Setup(); err != nil {
+		log.Fatal(err)
+	}
+	pin := gpio.New(opts.Pin)
+
+	logger := tools.NewStdoutLogger(tools.LogLevelDebug, "send")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf("> ")
+		scanner.Scan()
+
+		seq, err := gpio.ToSequence(scanner.Text())
+		if err != nil {
+			logger.Error(err)
+			continue
+		}
+
+		gpio.Execute(pin, seq)
+		response := gpio.Monitor(pin, Timeout)
+
+		logger.Infof("RESPONSE:")
+		logger.Infof(response.String())
+	}
+}
